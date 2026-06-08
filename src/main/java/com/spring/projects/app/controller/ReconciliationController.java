b264d8ceed8a4adc5a01b938691934ca0b276438
AYAN PRADHAN <ayanpradhan101@gmail.com>
2026-06-09 00:17:39 +0530

    refactor(batch controller):move refund endpoint to separate controller

--- src/main/java/com/spring/projects/app/controller/ReconciliationController.java
+++ src/main/java/com/spring/projects/app/controller/ReconciliationController.java
@@ -1,5 +1,11 @@
 package com.spring.projects.app.controller;
 
+import java.io.File;
+
+import org.springframework.batch.core.job.parameters.InvalidJobParametersException;
+import org.springframework.batch.core.launch.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.launch.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.launch.JobRestartException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,31 +13,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.spring.projects.app.dto.RefundDetails;
 import com.spring.projects.app.dto.Response;
+import com.spring.projects.app.exception.BatchJobUnsuccessfulException;
 import com.spring.projects.app.service.ReconciliationBatchService;
-import com.spring.projects.app.service.RefundService;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/service")
+@RequestMapping("/api/service/batch-job")
 public class ReconciliationController {
 	
 	private final ReconciliationBatchService batchService;
-	private final RefundService refundService;
 	
 	@GetMapping("/start-service")
-	public ResponseEntity<Response> startService() {
+	public ResponseEntity<Response> startService() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, InvalidJobParametersException, JobRestartException, BatchJobUnsuccessfulException {
 		return ResponseEntity.ok(batchService.startReconciliationService());
 	}
 	
-	@PostMapping("/initiate-refund")
-	public ResponseEntity<Response> initiateRefund(@RequestBody RefundDetails refundDetails) {
-		return ResponseEntity.ok(refundService.initiateManualRefund(refundDetails));
+	@PostMapping("/upload")
+	public ResponseEntity<Response> uploadFile(@RequestBody File file){
+		return null;
 	}
-
 	
+	
+
 	
 }
