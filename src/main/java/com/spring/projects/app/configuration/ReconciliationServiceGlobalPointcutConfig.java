2362fe6bef4858fb8615726eef2c7f23b5feec0a
AYAN PRADHAN <ayanpradhan101@gmail.com>
2026-06-08 21:00:07 +0530

    Logging aspect config added

---
+++ src/main/java/com/spring/projects/app/configuration/ReconciliationServiceGlobalPointcutConfig.java
@@ -0,0 +1,31 @@
+package com.spring.projects.app.configuration;
+
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ReconciliationServiceGlobalPointcutConfig {
+	
+	@Pointcut("execution(* com.spring.projects.app.service.RefundService.initiateBatchRefund())")
+	public void refundServiceBatchConfig() {};
+	
+	@Pointcut("execution(* com.spring.projects.app.service.RefundService.initiateManualRefund())")
+	public void refundServiceManualConfig() {};
+	
+	@Pointcut("execution(* com.spring.projects.app.service.ReportService.generateReport())")
+	public void reportGenerationServiceConfig() {};
+	
+	@Pointcut("execution(* com.spring.projects.app.service.ReportService.updateReportBatch())")
+	public void reportGenerationServiceBatchConfig() {};
+	
+	@Pointcut("execution(* com.spring.projects.app.service.ReportService.updateReportManual())")
+	public void reportGenerationServiceManualConfig() {};
+	
+	@Pointcut("execution(* com.spring.projects.app.service.BankRecordService.*(..))")
+	public void bankRecordServiceConfig() {};
+	
+	@Pointcut("execution(* com.spring.projects.app.service.ReconciliationBatchService.*(..))")
+	public void reconciliationBatchServiceConfig() {};
+	
+
+}
