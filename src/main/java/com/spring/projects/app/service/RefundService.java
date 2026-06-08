0d1b3b6f5f1db061b235c4f555091d12fb40d475
AYAN PRADHAN <ayanpradhan101@gmail.com>
2026-06-09 00:10:31 +0530

    perf(refund service): simplified and improved logic

--- src/main/java/com/spring/projects/app/service/RefundService.java
+++ src/main/java/com/spring/projects/app/service/RefundService.java
@@ -3,8 +3,11 @@ package com.spring.projects.app.service;
 import org.springframework.stereotype.Service;
 
 import com.spring.projects.app.constant.RefundStatus;
+import com.spring.projects.app.constant.ResponseStatusCode;
 import com.spring.projects.app.dto.RefundDetails;
 import com.spring.projects.app.dto.Response;
+import com.spring.projects.app.exception.RefundUnsuccessfulException;
+import com.spring.projects.app.exception.ReportModificationFaliureException;
 import com.spring.projects.app.repository.RefundRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -15,20 +18,22 @@ public class RefundService {
 	
 	private final RefundRepository refundRepo;
 	private final ReportService reportService;
-
-	public void initiateBatchRefund() {
-		if(refundRepo.insert() > 0 && refundRepo.updateBatch(RefundStatus.SUCCESS) > 0) {
-			reportService.updateReportBatch(RefundStatus.SUCCESS);
-		}
+	
+	public void initiateBatchRefund() throws RefundUnsuccessfulException, ReportModificationFaliureException {
+		if(refundRepo.insert() > 0 && refundRepo.updateBatch(RefundStatus.SUCCESS) > 0) 
+			if(reportService.updateReportBatch(RefundStatus.SUCCESS) > 0) 
+				return;
+			
+		throw new RefundUnsuccessfulException("No records found to initiate batch refund");
 	}
 	
-	public Response initiateManualRefund(RefundDetails refundDetails) {
+	public Response initiateManualRefund(RefundDetails refundDetails) throws RefundUnsuccessfulException, ReportModificationFaliureException {
 		if(refundRepo.update(refundDetails) > 0) {
-			reportService.updateReportManual(refundDetails);
-			return new Response("SUCCESS","{}");
+			long code = reportService.updateReportManual(refundDetails);
+			return new Response(ResponseStatusCode.SUCCESS,"Refund is successful with exit code  " + code);
 		}
-		
-		return new Response("","");
+        throw new RefundUnsuccessfulException("Refund unsucccessful for Txn Id: " + refundDetails.txnId());
 	}
 	
 }
+ 
